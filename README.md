8210b36198c8e279eb601677272181e903578735
Chris <chris@mm4solutions.com>
2017-06-29 10:59:44 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# md partitions-feature-projects
+# MD Partitions Feature Projects
+
+Everything you should need for this is below. I have not grabbed anything in the way of styling. You will need to grab that from the [GitHub page](https://github.com/rejas/imagelightbox). I have the most recent version of the main script [here](lightbox.js).
+
+1. [Import the ACF fields](feature-projects.json)
+2. Add [these functions](functions.php) to your functions.php file
+
+Call the function in your template file:
+```php
+if( function_exists('md_partitions_project_listing') ): md_partitions_project_listing(); endif;
+```
---
+++ functions.php
@@ -0,0 +1,61 @@
+<?php
+function md_partitions_project_listing() {
+	if(function_exists('get_field')) {
+		if( have_rows('project_listing') ):
+			// You will probably want to change this path to a minified version of the script
+			wp_enqueue_script( 'md-partitions-lightbox', get_template_directory_uri() . '/js/lightbox.js', array( 'jquery'), NULL, TRUE );
+			$i = 0; ?>
+			<section class="featured-projects">
+				<h2><span>Featured Projects</span></h2>
+				<ul>
+					<?php 
+					while( have_rows('project_listing') ): the_row();
+					$i++;
+					$name = get_sub_field('project_name');
+					$thumb = get_sub_field('project_main_image');
+					$gallery_imgs = get_sub_field('project_gallery'); ?>
+					<li class="trigger-gallery-<?php echo $i; ?>">
+						<img src="<?php echo esc_url( $thumb['sizes']['thumbnail'] ); ?>" alt="<?php echo esc_attr( $thumb['alt'] ); ?>">
+						<?php echo esc_html( $name ); ?>
+						<?php // You might want to remove the inline CSS and add to your SASS files ?>
+						<div class="gallery-images" style="display:none">
+							<ul>
+								<?php foreach( $gallery_imgs as $img ): ?>
+									<li><a href="<?php echo esc_url( $img['url'] ); ?>" data-imagelightbox="gallery-<?php echo $i; ?>"><?php echo esc_html( $img['filename'] ); ?></a></li>
+								<?php endforeach; ?>
+							</ul>
+						</div>
+					</li>
+					<?php endwhile; ?>
+				</ul>
+			</section>
+		<?php endif;
+	}
+}
+
+add_action( 'wp_footer', 'md_partitions_build_project_script', 100, 1 );
+function md_partitions_build_project_script() {
+	if(function_exists('get_field')) { 
+		if( have_rows('project_listing') ):
+			$i = 0; ?>
+			<script>
+			<?php while( have_rows('project_listing') ): the_row();
+				$i++;
+				// Feel free to change the options below. You can find them here: https://github.com/rejas/imagelightbox ?>
+				var gallery<?php echo $i; ?> = jQuery('a[data-imagelightbox="gallery-<?php echo $i; ?>"]').imageLightbox({
+					activity: true,
+					arrows: true,
+					button:true,
+					lockbody:true,
+					overlay:true,
+					preloadNext: true
+				});
+
+				jQuery('.trigger-gallery-<?php echo $i; ?>').on('click', function () {
+					gallery<?php echo $i; ?>.startImageLightbox();
+				});
+			<?php endwhile; ?>
+			</script>
+		<?php endif; 
+	}
+}
